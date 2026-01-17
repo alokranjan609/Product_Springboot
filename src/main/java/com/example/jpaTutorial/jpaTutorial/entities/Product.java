fbe6069ba26262b79f8043fb235a9ed5e7f3bbf4
Alok <alokthakur609@gmail.com>
2026-01-17 11:58:42 +0530

    first commit

---
+++ src/main/java/com/example/jpaTutorial/jpaTutorial/entities/Product.java
@@ -0,0 +1,28 @@
+package com.example.jpaTutorial.jpaTutorial.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String title;
+    private BigDecimal price;
+    private  Integer quantity;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+}
